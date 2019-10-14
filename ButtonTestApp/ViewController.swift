323749511676c7047fb5fd6b2b5f057f8934b6b8
iori1 <sf5iori@gmail.com>
2019-10-14 14:14:24 +0900

    first commit

--- ButtonTestApp/ViewController.swift
+++ ButtonTestApp/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func tapButton(_ sender: Any) {
+        textLabel.text = "Hello World"
+        
+        
+    }
+    
 
 }
 
